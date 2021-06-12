ef48c3efbf2e2953042b0b08b8c308b6c8dc35b6
azeez1776 <aboud3am81@gmail.com>
2021-06-12 18:39:57 +0300

    Initial commit

---
+++ include/db.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', 'samatar@1776', 'transfer');
+
+if (!$conn) {
+    die('Error Connection');
+}
---
+++ registration/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../resources/css/bootstrap.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container d-flex">
+
+            <a class="navbar-brand" href="#">T-Transfer</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-right" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+    <h1>Login Page</h1>
+
+
+    <script src="../resources/js/bootstrap.js"></script>
+</body>
+
+</html>
